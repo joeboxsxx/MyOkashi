9e2fba67634c1e4708928357435d67ef022842fe
joeboxsxx <joeboxsxx@ezweb.ne.jp>
2023-03-07 01:36:18 +0900

    Implementation of asynchronous processing

--- MyOkashi/OkashiData.swift
+++ MyOkashi/OkashiData.swift
@@ -13,5 +13,18 @@ class OkashiData: ObservableObject {
     func searchOkashi(keyword: String) {
         // デバックエリアに出力
         print("searchOkashiメソッドで受け取った値：\(keyword)")
+        
+        // Taskは非同期で処理を実行できる
+        Task {
+            // ここから先は非同期で処理される
+            // 非同期でお菓子を検索する
+            await search(keyword: keyword)
+        } // end Task
     } // end searchOkashi
+    
+    // 非同期でお菓子データを取得
+    private func search(keyword: String) async {
+        // デバックエリアに出力
+        print("searchメソッドで受け取った値：\(keyword)")
+    } // end search
 } // end OkashiData
