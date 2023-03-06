0d64f48d66a2432235d0f8afa0a6fa5fc1d161cc
joeboxsxx <joeboxsxx@ezweb.ne.jp>
2023-03-07 00:47:29 +0900

    Initial Commit

---
+++ MyOkashi/MyOkashiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyOkashiApp.swift
+//  MyOkashi
+//
+//  Created by 戸上健太 on 2023/03/07.
+//
+
+import SwiftUI
+
+@main
+struct MyOkashiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
